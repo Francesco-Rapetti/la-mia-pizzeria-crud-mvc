b02353a96465c5f78e1a5fb9e2e65ae5ce4dac9e
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-15 15:10:07 +0200

    Aggiungere i file di progetto.

---
+++ Pizzeria/Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace Pizzeria.Models
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+
+        public Pizza(string name, string description, double price, string image = "/img/pizza-placeholder.png")
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Image = image;
+            this.Price = price;
+        }
+    }
+}
---
+++ Pizzeria/Models/Pizzeria.cs
@@ -0,0 +1,21 @@
+﻿namespace Pizzeria.Models
+{
+    public class Pizzeria
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public List<Pizza> Pizzas { get; set; }
+
+        public Pizzeria(string name, string description, string image = "https://static.vecteezy.com/system/resources/previews/002/977/249/original/pizzeria-store-front-illustration-in-flat-style-vector.jpg")
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Image = image;
+            this.Pizzas = new List<Pizza>();
+        }
+
+        public void AddPizza(Pizza pizza) => Pizzas.Add(pizza);
+        public void RemovePizza(Pizza pizza) => Pizzas.Remove(pizza);
+    }
+}
