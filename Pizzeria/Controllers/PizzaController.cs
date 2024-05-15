025ebf4f652d9d5a61fdc0565649dce37e64b436
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-15 18:18:54 +0200

    Add PizzaController

---
+++ Pizzeria/Controllers/PizzaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pizzeria_project.Controllers
+{
+    public class PizzaController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+}
