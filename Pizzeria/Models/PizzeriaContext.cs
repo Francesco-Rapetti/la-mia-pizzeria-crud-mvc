885519952b6a5588466471368e68231b624bc8f3
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-15 16:13:31 +0200

    Add PizzeriaContext

---
+++ Pizzeria/Models/PizzeriaContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pizzeria.Models
+{
+    public class PizzeriaContext : DbContext
+    {
+        public DbSet<string> Name { get; set; }
+        public DbSet<string> Description { get; set; }
+        public DbSet<string> Image { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=Pizzeria;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+        }
+    }
+}
