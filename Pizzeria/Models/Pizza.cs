7920bcb8f3a0121716c572d5dd62ed82aea1b6c9
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-16 19:12:06 +0200

    Add validation

--- Pizzeria/Models/Pizza.cs
+++ Pizzeria/Models/Pizza.cs
@@ -1,11 +1,31 @@
-﻿namespace pizzeria_project.Models
+﻿using pizzeria_project.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace pizzeria_project.Models
 {
     public class Pizza
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name")]
+        [Display(Name = "Pizza Name")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a description")]
+        [Display(Name = "Pizza Description")]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        [MinNWordsValidation(5)]
         public string Description { get; set; }
+
+        //[Required(ErrorMessage = "Please enter an image")]
+        //[Display(Name = "Pizza Image")]
+        //[StringLength(500, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
         public string Image { get; set; }
+
+        [PriceValidation]
+        [Required(ErrorMessage = "Please enter a price")]
+        [Display(Name = "Pizza Price")]
         public double Price { get; set; }
 
         public Pizza()
@@ -18,7 +38,7 @@
             this.Name = name;
             this.Description = description;
             this.Image = image;
-            this.Price = price;
+            this.Price = Math.Round(price, 2);
         }
     }
 }
