96a0bbd22eb9239364e177c76a81e280d8fcd45a
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-20 16:54:05 +0200

    Add PizzaFormModel

---
+++ Pizzeria/Models/PizzaFormModel.cs
@@ -0,0 +1,8 @@
+﻿namespace pizzeria_project.Models
+{
+    public class PizzaFormModel
+    {
+        public Pizza Pizza { get; set; }
+        public List<Category>? Categories { get; set; }
+    }
+}
