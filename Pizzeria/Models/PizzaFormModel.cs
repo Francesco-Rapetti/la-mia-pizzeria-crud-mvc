eb565a9032943a4299438cc2823ae69348b72080
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-21 20:20:04 +0200

    Add ingredients to PizzaFormModel

--- Pizzeria/Models/PizzaFormModel.cs
+++ Pizzeria/Models/PizzaFormModel.cs
@@ -1,8 +1,16 @@
-﻿namespace pizzeria_project.Models
+﻿using pizzeria_project.Validations;
+
+namespace pizzeria_project.Models
 {
     public class PizzaFormModel
     {
-        public Pizza Pizza { get; set; }
+        public Pizza Pizza { get; set; } = new Pizza();
         public List<Category>? Categories { get; set; }
+        public List<Ingredient>? Ingredients { get; set; }
+
+        [PizzaIngredientsValidation]
+        public List<int>? SelectedIngredientsIds { get; set; } = new List<int>();
+
+
     }
 }
