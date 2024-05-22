f08fadb954cfc9f279de1d2522cf4cba9f63f765
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-22 17:42:46 +0200

    Add identity tables

--- Pizzeria/Migrations/20240522123000_CreateIdentityTables.Designer.cs
+++ Pizzeria/Migrations/20240522123000_CreateIdentityTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using pizzeria_project;
 
@@ -11,9 +12,11 @@ using pizzeria_project;
 namespace pizzeriaproject.Migrations
 {
     [DbContext(typeof(PizzaContext))]
-    partial class PizzaContextModelSnapshot : ModelSnapshot
+    [Migration("20240522123000_CreateIdentityTables")]
+    partial class CreateIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
