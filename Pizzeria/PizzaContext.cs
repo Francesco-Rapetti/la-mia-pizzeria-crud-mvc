c53a31f5ad4b5d44bc88dae92236f0dcfce85c7f
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-22 17:42:57 +0200

    Add Authorization

--- Pizzeria/PizzaContext.cs
+++ Pizzeria/PizzaContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using pizzeria_project.Models;
 
 namespace pizzeria_project
 {
-    public class PizzaContext : DbContext
+    public class PizzaContext : IdentityDbContext<IdentityUser>
     {
         public DbSet<Pizza> Pizzas { get; set; }
         public DbSet<Category> Categories { get; set; }
